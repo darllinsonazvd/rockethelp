7553c3c70b720ee85405552aae6ee3c7ec2e9ff5
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-07-21 09:58:18 -0300

    Screens finished

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/styles/theme';
 
-import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+import { Register } from './src/screens/Register';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -12,7 +14,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Register /> : <Loading />}
     </NativeBaseProvider>
   );
 }
